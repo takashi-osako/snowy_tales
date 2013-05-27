b51f29f47b339e2bcdd3cfe8148cf8ce9bcac94f
Doris Ip <dorisip@gmail.com>
2013-05-26 21:46:43 -0400

    Use BufferedInputStream to read file

--- PDFGenerator/src/main/java/snowy_tales/reader/FileReader.java
+++ PDFGenerator/src/main/java/snowy_tales/reader/FileReader.java
@@ -1,26 +1,36 @@
 package snowy_tales.reader;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class FileReader {
-	public static byte[] readFile(String fileName) {
+	public static byte[] readFile(String fileName) throws IOException {
 		File file = new File(fileName);
-		byte [] buffer =null;  
+		byte[] buffer = null;
+		BufferedInputStream bis = null;
+		FileInputStream fin = null;
 		try {
-			// TODO: don't read the file all at once?
-			FileInputStream in = new FileInputStream(file);
-			int length = (int)file.length();
-			buffer = new byte [length];
-			in.read(buffer);
-			in.close();
-			 
+			fin = new FileInputStream(file);
+			int length = (int) file.length();
+			bis = new BufferedInputStream(fin);
+			buffer = new byte[length];
+
+			bis.read(buffer);
+
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found" + e);
 		} catch (IOException ioe) {
 			System.out.println("Exception while reading the file " + ioe);
+		} finally {
+			if (bis != null) {
+				bis.close();
+			}
+			if (fin != null) {
+				fin.close();
+			}
 		}
 		return buffer;
 	}
